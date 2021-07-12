d2a83bdb5424ba3e9a84c4485345e86796f042cf
Sayansree Paria <31517759+Sayansree@users.noreply.github.com>
2021-07-13 01:38:39 +0530

    Create 5. Rotate Array.cpp

---
+++ Day02/5. Rotate Array.cpp
@@ -0,0 +1,43 @@
+/***
+5. Rotate Array 
+Easy Accuracy: 50.28% Submissions: 65353 Points: 2
+Given an unsorted array arr[] of size N, rotate it by D elements in the counter-clockwise direction. 
+
+ 
+
+Example 1:
+
+Input:
+N = 5, D = 2
+arr[] = {1,2,3,4,5}
+Output: 3 4 5 1 2
+Explanation: 1 2 3 4 5  when rotated
+by 2 elements, it becomes 3 4 5 1 2.
+Example 2:
+
+Input:
+N = 10, D = 3
+arr[] = {2,4,6,8,10,12,14,16,18,20}
+Output: 8 10 12 14 16 18 20 2 4 6
+Explanation: 2 4 6 8 10 12 14 16 18 20 
+when rotated by 3 elements, it becomes 
+8 10 12 14 16 18 20 2 4 6.
+ 
+
+Your Task:
+Complete the function rotateArr() which takes the array, D and N as input parameters and rotates the array by D elements. The array must be modified in-place without using extra space. 
+
+ 
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+ 
+
+Constraints:
+1 <= N <= 107
+1 <= D <= N
+0 <= arr[i] <= 105
+
+
+**/
