833ef0464ed035643b36e4a905f7c96532564673
Sayansree Paria <31517759+Sayansree@users.noreply.github.com>
2021-07-12 12:31:09 +0530

    Create 1.Trailing zeroes in factorial.cpp

---
+++ Day01/1.Trailing zeroes in factorial.cpp
@@ -0,0 +1,29 @@
+/******
+  Trailing zeroes in factorial 
+  For an integer N find the number of trailing zeroes in N!.
+
+  Example 1:
+
+  Input:
+  N = 5
+  Output:
+  1
+  Explanation:
+  5! = 120 so the number of trailing zero is 1.
+  Example 2:
+
+  Input:
+  N = 4
+  Output:
+  0
+  Explanation:
+  4! = 24 so the number of trailing zero is 0.
+  Your Task:  
+  You don't need to read input or print anything. Your task is to complete the function trailingZeroes() which take an integer N as an input parameter and returns the count of trailing zeroes in the N!.
+
+  Expected Time Complexity: O(logN)
+  Expected Auxiliary Space: O(1)
+
+  Constraints:
+  1 <= N <= 109
+****/
