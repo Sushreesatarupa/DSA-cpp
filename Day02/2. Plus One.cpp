3126536e2a09168b0db9e693baf49702b72183d6
Sayansree Paria <31517759+Sayansree@users.noreply.github.com>
2021-07-13 01:33:52 +0530

    Create 2. Plus One.cpp

---
+++ Day02/2. Plus One.cpp
@@ -0,0 +1,38 @@
+/**
+2. Plus One 
+Given a non-negative number represented as a list of digits, add 1 to the number (increment the number represented by the digits). The digits are stored such that the most significant digit is first element of array.
+ 
+
+Example 1:
+
+Input: 
+N = 3
+arr[] = {1, 2, 4}
+Output: 
+1 2 5
+Explanation:
+124+1 = 125, and so the Output
+Example 2:
+
+Input: 
+N = 3
+arr[] = {9,9,9}
+Output: 
+1 0 0 0
+Explanation:
+999+1 = 1000, and so the output
+
+Your Task:
+You don't need to read input or print anything. You only need to complete the function increment() that takes an integer N, and an array arr of size N as input and returns a list of integers denoting the new number which we get after adding one to the number denoted by the array arr.
+
+
+Expected Time Complexity:  O(N)
+Expected Auxilliary Space: O(1)
+ 
+
+Constraints:
+1 <= N <= 105
+0 <= arri <= 9
+
+
+**/
