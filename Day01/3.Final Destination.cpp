d624e6c44a9f61f6d563cdd90a6148326e6d4586
Sayansree Paria <31517759+Sayansree@users.noreply.github.com>
2021-07-12 13:44:49 +0530

    Update 3.Final Destination.cpp

--- Day01/3.Final Destination.cpp
+++ Day01/3.Final Destination.cpp
@@ -39,3 +39,30 @@ Constraints:
 1 <= x <= 2*109
 
 **/
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    int canReach(long long a, long long b, long long x) {
+        // code here
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long a,b,x;
+        
+        cin>>a>>b>>x;
+
+        Solution ob;
+        cout << ob.canReach(a,b,x) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
