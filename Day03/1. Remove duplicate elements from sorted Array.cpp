151004d2c4a5c043b8523e068e7fe6a41cc93671
Sayansree Paria <31517759+Sayansree@users.noreply.github.com>
2021-07-13 13:28:59 +0530

    Update 1. Remove duplicate elements from sorted Array.cpp

--- Day03/1. Remove duplicate elements from sorted Array.cpp
+++ Day03/1. Remove duplicate elements from sorted Array.cpp
@@ -32,3 +32,42 @@ Constraints:
 1 ≤ N ≤ 104
 1 ≤ A[i] ≤ 106
 **/
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    int remove_duplicate(int a[],int n){
+        // code here
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int N;
+        cin>>N;
+        int a[N];
+        for(int i=0;i<N;i++)
+        {
+            cin>>a[i];
+        }
+    Solution ob;
+    int n = ob.remove_duplicate(a,N);
+
+    for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+    }
+}  // } Driver Code Ends
