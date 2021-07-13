96d04a48ebc28e03e5c2dfbec481e7032f04c719
Sayansree Paria <31517759+Sayansree@users.noreply.github.com>
2021-07-13 13:21:52 +0530

    Update 5. Largest Number formed from an Array .cpp

--- Day03/5. Largest Number formed from an Array .cpp
+++ Day03/5. Largest Number formed from an Array .cpp
@@ -30,3 +30,39 @@ Constraints:
 1 ≤ N ≤ 105
 0 ≤ Arr[i] ≤ 1018
 ***/
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	// The main function that returns the arrangement with the largest value as
+	// string.
+	// The function accepts a vector of strings
+	string printLargest(vector<string> &arr) {
+	    // code here
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        vector<string> arr(n);
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.printLargest(arr);
+        cout << ans << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
