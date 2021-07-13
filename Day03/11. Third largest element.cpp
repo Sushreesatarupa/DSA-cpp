00ce7dd87d08b05f9bc5d740f533b1578600438e
Sayansree Paria <31517759+Sayansree@users.noreply.github.com>
2021-07-13 12:56:05 +0530

    Create 11. Third largest element.cpp

---
+++ Day03/11. Third largest element.cpp
@@ -0,0 +1,27 @@
+/**
+11. Third largest element 
+Basic Accuracy: 46.52% Submissions: 36544 Points: 1
+Given an array of distinct elements. Find the third largest element in it. 
+
+Example 1:
+
+Input:
+N = 5
+A[] = {2,4,1,3,5}
+Output: 3
+Example 2:
+
+Input:
+N = 2
+A[] = {10,2}
+Output: -1
+Your Task:
+Complete the function thirdLargest() which takes the array a[] and the size of the array, n, as input parameters and returns the third largest element in the array. It return -1 if there are less than 3 elements in the given array.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ N ≤ 105
+1 ≤ A[i] ≤ 105
+**/
