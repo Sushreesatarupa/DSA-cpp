49f1ad63970fb589a15802ecc8e2f2ead055fac0
Sayansree Paria <31517759+Sayansree@users.noreply.github.com>
2021-07-13 13:33:22 +0530

    Update 2. Length Unsorted Subarray.cpp

--- Day02/2. Length Unsorted Subarray.cpp
+++ Day02/2. Length Unsorted Subarray.cpp
@@ -38,3 +38,37 @@ Constraints:
 
 
 **/
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	vector<int> printUnsorted(int arr[], int n) {
+	    // code here
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.printUnsorted(arr, n);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
