29eab1f41a29aadef9c65ee88e31b366f3d1c4cd
Sayansree Paria <31517759+Sayansree@users.noreply.github.com>
2021-07-13 13:34:28 +0530

    Update 3. Product array puzzle.cpp

--- Day02/3. Product array puzzle.cpp
+++ Day02/3. Product array puzzle.cpp
@@ -39,3 +39,51 @@ Constraints:
 0 <= numsi <= 200
 Array may contain duplicates.
 **/
+// { Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    // nums: given vector
+    // return the Product vector P that hold product except self at each index
+    vector<long long int> productExceptSelf(vector<long long int>& nums, int n) {
+       
+        //code here        
+    }
+};
+
+
+// { Driver Code Starts.
+int main()
+ {
+    int t;  // number of test cases
+    cin>>t;
+    while(t--)
+    {
+        int n;  // size of the array
+        cin>>n;
+        vector<long long int> arr(n),vec(n);
+        
+        for(int i=0;i<n;i++)    // input the array
+        {
+            cin>>arr[i];
+        }
+        Solution obj;
+        vec = obj.productExceptSelf(arr,n);   // function call
+        
+        for(int i=0;i<n;i++)    // print the output
+        {
+            cout << vec[i] << " ";
+        }
+        cout<<endl;
+    }
+	return 0;
+}  // } Driver Code Ends
