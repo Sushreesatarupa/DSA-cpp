dc603654dbd04ea5e2fffd1cb9c35fd4d6e81284
Sayansree Paria <31517759+Sayansree@users.noreply.github.com>
2021-07-13 13:24:58 +0530

    Update 4. Frequencies of Limited Range Array Elements.cpp

--- Day03/4. Frequencies of Limited Range Array Elements.cpp
+++ Day03/4. Frequencies of Limited Range Array Elements.cpp
@@ -49,3 +49,56 @@ Constraints:
 1 <= A[i] <= P
 
 ***/
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std; 
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    //Function to count the frequency of all elements from 1 to N in the array.
+    void frequencyCount(vector<int>& arr,int N, int P)
+    { 
+        // code here
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+	long long t;
+	
+	//testcases
+	cin >> t;
+	
+	while(t--){
+	    
+	    int N, P;
+	    //size of array
+	    cin >> N; 
+	    
+	    vector<int> arr(N);
+	    
+	    //adding elements to the vector
+	    for(int i = 0; i < N ; i++){
+	        cin >> arr[i]; 
+	    }
+        cin >> P;
+        Solution ob;
+        //calling frequncycount() function
+		ob.frequencyCount(arr, N, P); 
+		
+		//printing array elements
+	    for (int i = 0; i < N ; i++) 
+			cout << arr[i] << " ";
+	    cout << endl;
+	}	
+	return 0; 
+}
+
+
+
+
+  // } Driver Code Ends
