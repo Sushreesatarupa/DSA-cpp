7597185ab499c9b5ed288b550db13865d9e0b114
Sayansree Paria <31517759+Sayansree@users.noreply.github.com>
2021-07-13 13:29:48 +0530

    Update 3. Alternate positive and negative numbers.cpp

--- Day03/3. Alternate positive and negative numbers.cpp
+++ Day03/3. Alternate positive and negative numbers.cpp
@@ -32,3 +32,40 @@ Constraints:
 1 ≤ N ≤ 107
 -106 ≤ Arr[i] ≤ 107
 **/
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+
+	void rearrange(int arr[], int n) {
+	    // code here
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.rearrange(arr, n);
+        for (i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
