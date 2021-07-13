003f4a8d539bf81e679a63bc8f8a0a979df58312
Dibyansika <72373564+Dibyansika@users.noreply.github.com>
2021-07-13 22:53:47 +0530

    Update 5. Rotate Array.cpp

--- Day02/5. Rotate Array.cpp
+++ Day02/5. Rotate Array.cpp
@@ -50,10 +50,21 @@ using namespace std;
 
 class Solution{
     public:
-    
     //Function to rotate an array by d elements in counter-clockwise direction. 
     void rotateArr(int arr[], int d, int n){
         // code here
+	int temp[d];
+        for(int i=0; i<d; i++)
+        {
+            temp[i]= arr[i];
+        }
+        for(int i=d; i<n; i++)
+        {
+            arr[i-d]= arr[i];
+        }
+        for(int i=0; i<d;i++){
+            arr[n-d+i]= temp[i];
+        }
     }
 };
 
