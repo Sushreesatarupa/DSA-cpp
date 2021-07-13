8f142739f29bc5f480bf01ddc93b069a96d6b16e
Sayansree Paria <31517759+Sayansree@users.noreply.github.com>
2021-07-13 13:31:49 +0530

    Update 1. Plus One.cpp

--- Day02/1. Plus One.cpp
+++ Day02/1. Plus One.cpp
@@ -36,3 +36,40 @@ Constraints:
 
 
 **/
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+
+	void rearrange(int arr[], int n) {
+	    // code here
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.rearrange(arr, n);
+        for (i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
