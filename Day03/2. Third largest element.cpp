28dfbd2b7261870fbce6c2fef2c476e123b3f762
Sayansree Paria <31517759+Sayansree@users.noreply.github.com>
2021-07-13 13:27:21 +0530

    Update 2. Third largest element.cpp

--- Day03/2. Third largest element.cpp
+++ Day03/2. Third largest element.cpp
@@ -1,6 +1,5 @@
 /**
 2. Third largest element 
-Basic Accuracy: 46.52% Submissions: 36544 Points: 1
 Given an array of distinct elements. Find the third largest element in it. 
 
 Example 1:
@@ -25,3 +24,61 @@ Constraints:
 1 ≤ N ≤ 105
 1 ≤ A[i] ≤ 105
 **/
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std; 
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    //Function to count the frequency of all elements from 1 to N in the array.
+    void frequencyCount(vector<int>& arr,int N, int P)
+    { 
+        vector <int> arr2(P,0) ;
+        for(int i =0;i<N;i++)
+            arr2.push_back(0);
+        for(int i=0;i<N;i++)
+            arr2[arr[i]-1]++;
+        arr=arr2;// code here
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+	long long t;
+	
+	//testcases
+	cin >> t;
+	
+	while(t--){
+	    
+	    int N, P;
+	    //size of array
+	    cin >> N; 
+	    
+	    vector<int> arr(N);
+	    
+	    //adding elements to the vector
+	    for(int i = 0; i < N ; i++){
+	        cin >> arr[i]; 
+	    }
+        cin >> P;
+        Solution ob;
+        //calling frequncycount() function
+		ob.frequencyCount(arr, N, P); 
+		
+		//printing array elements
+	    for (int i = 0; i < N ; i++) 
+			cout << arr[i] << " ";
+	    cout << endl;
+	}	
+	return 0; 
+}
+
+
+
+
+  // } Driver Code Ends
