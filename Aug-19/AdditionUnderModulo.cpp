405d837b37a526d17d840e27b9ccc5eb9918eca2
Sushree Satarupa <64991656+Sushreesatarupa@users.noreply.github.com>
2021-08-19 10:14:58 +0530

    Create AdditionUnderModulo.cpp

---
+++ Aug-19/AdditionUnderModulo.cpp
@@ -0,0 +1,32 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+        long long sumUnderModulo(long long a,long long b)
+    {
+        // code here
+        return(a%1000000007+b%1000000007)%1000000007;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    long long a;
+	    long long b;
+	    cin>>a>>b;
+	    Solution ob;
+	    cout<<ob.sumUnderModulo(a,b)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
