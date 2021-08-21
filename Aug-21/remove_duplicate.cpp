c01b1f0fc8bf2dc44eae76c6e3e17445d1e6b84e
Sushree Satarupa <64991656+Sushreesatarupa@users.noreply.github.com>
2021-08-21 10:33:11 +0530

    Create remove_duplicate.cpp

---
+++ Aug-21/remove_duplicate.cpp
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    int remove_duplicate(int a[],int n){
+        // code here
+        int i,j;
+        for(i=1,j=0;i<n;i++){
+            if(a[i]!=a[j]){
+                j++;
+                a[j]=a[i];
+            }
+        }
+        n=j+1;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int N;
+        cin>>N;
+        int a[N];
+        for(int i=0;i<N;i++)
+        {
+            cin>>a[i];
+        }
+    Solution ob;
+    int n = ob.remove_duplicate(a,N);
+
+    for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+    }
+}  // } Driver Code Ends
