935dac41d73c63dd087fca476a9343704a2346b3
Swayam Gupta <68892315+Swayam221@users.noreply.github.com>
2021-10-01 00:34:34 +0530

    added question statement

--- 03. Bit Magic/element-appearing-once.cpp
+++ 03. Bit Magic/element-appearing-once.cpp
@@ -1,3 +1,30 @@
+/* Question:
+    Given a sorted array A[] of N positive integers having all the numbers occurring exactly twice, 
+    except for one number which will occur only once. Find the number occurring only once.
+    
+    Example 1:
+
+    Input:
+    N = 5
+    A = {1, 1, 2, 5, 5}
+    Output: 2
+    Explanation: 
+    Since 2 occurs once, while
+    other numbers occur twice, 
+    2 is the answer.
+    
+    Example 2:
+
+    Input:
+    N = 7
+    A = {2, 2, 5, 5, 20, 30, 30}
+    Output: 20
+    Explanation:
+    Since 20 occurs once, while
+    other numbers occur twice, 
+    20 is the answer.
+*/    
+
 #include<bits/stdc++.h>
 
 using namespace std;
