bdb698c80c489951948371985d20cc44d8a05a27
Utkarsh Agnihotri <30363000+agniutkarsh@users.noreply.github.com>
2021-10-01 00:21:31 +0530

    Create NextGreaterElement.cpp

---
+++ 12. Stack/NextGreaterElement.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+ 
+
+void printNGE(int arr[], int n)
+{
+    int next, i, j;
+    for (i = 0; i < n; i++)
+    {
+        next = -1;
+        for (j = i + 1; j < n; j++)
+        {
+            if (arr[i] < arr[j])
+            {
+                next = arr[j];
+                break;
+            }
+        }
+        cout << arr[i] << " -- "
+             << next << endl;
+    }
+}
+ 
+int main()
+{
+    int arr[] = {11, 13, 21, 3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printNGE(arr, n);
+    return 0;
+}
