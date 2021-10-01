1fb467d14375c92e31cdf7074dc0c2aee9076cfe
O-alpha <78210614+O-alpha@users.noreply.github.com>
2021-10-01 21:42:27 +0530

    Update maxindicesdifference.cpp

--- 05. Arrays/maxindicesdifference.cpp
+++ 05. Arrays/maxindicesdifference.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int i = -1, j, n, p1 = 0, p2;
+    int i = -1, j, n, p1 = 0, p2;// p1 and p2 keep index values of the 2 elements
     cout << "Enter number of elements: ";
     cin >> n;
     cout << "Enter the elements-\n";
@@ -15,8 +15,8 @@ int main()
     j = n;
 
     while (++i < n / 2 && --j >= n / 2)
-        if (a[i] < a[j])
-        {
+        if (a[i] < a[j]) //p1 and p2 are intially found by simultaneously traversing the array from the front and end
+        {                
             p1 = i;
             p2 = j;
         }
@@ -24,12 +24,12 @@ int main()
     i = p1;
     j = p2;
     while (i-- >= 0)
-        if (a[i] < a[p2])
+        if (a[i] < a[p2]) //array is traversed from p1-1 to 0 to find an element < a[p2]
             p1 = i;
 
-    while (++j < n)
+    while (++j < n) //array is traversed from p2+1 to n-1 to find an element > a[p1]
         if (a[p1] < a[j])
             p2 = j;
     cout << "Max difference: \n";
-    cout << p2 - p1 << endl;
+    cout << p2 - p1 << endl; //max diff is found
 }
