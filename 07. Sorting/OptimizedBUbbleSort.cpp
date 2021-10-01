b6756d10abcb51dab5a4ae1198672f7f3c2d3494
Abhijeet Sharma <60298937+abhijeetsharma20@users.noreply.github.com>
2021-10-01 19:12:46 +0530

    Update OptimizedBUbbleSort.cpp

--- 07. Sorting/OptimizedBUbbleSort.cpp
+++ 07. Sorting/OptimizedBUbbleSort.cpp
@@ -2,18 +2,20 @@
 #include<vector>
 using namespace std;
 
+// function for bubble sort
 vector <int> optimizedBubbleSort(vector<int> v){
     // your code  goes here
-    int flag;
+    int flag; // declaring a variable which will keep track if the swaping is done or not
     for(int i=0;i<v.size()-1;i++){
-        flag=0;
+        flag=0; // intializing flag with value 0
         for(int j=0;j<v.size()-1-i;j++){
             if(v[j]>v[j+1]) {
                 swap(v[j], v[j+1]);
                 flag=1;
             }        
     }
-    if (flag==0) break;
+    if (flag==0) break; // if no swapping done that means array is already sorted
     }
+    
     
 }
