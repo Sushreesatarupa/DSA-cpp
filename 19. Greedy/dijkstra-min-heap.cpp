42e102835274c6eebe47bbbc4f469ee743aff064
navjotdadwal <59385365+navjotdadwal@users.noreply.github.com>
2021-10-01 10:34:14 +0530

    Update dijkstra-min-heap.cpp

--- 19. Greedy/dijkstra-min-heap.cpp
+++ 19. Greedy/dijkstra-min-heap.cpp
@@ -1,59 +1,33 @@
 #include<bits/stdc++.h>
-#define int long long int
-#define INF 10000000000000
 #define pii pair<int,int>
+#define INF 10000000
 using namespace std;
-vector<pair<int,int>> adj[100001];
-vector<int> dist,path;
-void dijkstra(int n)
+vector<pii> adj[10001];
+int n;
+int dijkstra(int src,int dest)
 {
-  priority_queue<int> pq;
-  pq.push(1);
-  dist[1]=0;
+  int dist[n+1];
+  for(int i=0;i<n+1;i++)
+    dist[i]=INF;
+  priority_queue<pii,vector<pii>,greater<pii>> pq;
+  pq.push({0,src});
+  dist[src]=0;
   while(!pq.empty())
   {
-    int node=pq.top();
+    int x=pq.top().first;
+    int y=pq.top().second;
     pq.pop();
-    for(auto child: adj[node])
+    if(y==dest)
+        return dist[dest];
+    for(auto child: adj[y])
     {
-      if(dist[node]+child.second<dist[child.first])
-      {
-        path[child.first]=node;
-        dist[child.first]=dist[node]+child.second;
-        pq.push(child.first);
-      }
+       if(child.second+dist[y]<dist[child.first])
+       {
+         dist[child.first]=child.second+dist[y];
+         pq.push({dist[child.first],child.first});
+       }
     }
   }
+  return dist[dest];
 }
-main()
-{
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
-  int n,m;
-  cin>>n>>m;
-  for(int i=0;i<m;i++)
-  {
-    int a,b,w;
-    cin>>a>>b>>w;
-    adj[a].push_back({b,w});
-    adj[b].push_back({a,w});
-  }
-  dist.assign(n+1,INF);
-  path.assign(n+1,0);
-  dijkstra(n);
-  if(dist[n]==INF)
-  {
-    cout<<"-1\n";return 0;
-  }
-  vector<int> v;
-  int x=n;
-  v.push_back(n);
-  while(x!=1)
-  {
-    x=path[x];
-    v.push_back(x);
-  }
-  for(int i=v.size()-1;i>=0;i--)
-    cout<<v[i]<<" ";
-}
+
