56e3581b78232b1b44f39b63ca9143da74f148c9
vivekananda99 <91697157+vivekananda99@users.noreply.github.com>
2021-10-01 11:17:40 +0530

    Created sieve_of_eratosthenes.cpp
    C++ program to print all primes smaller than or equal to n using Sieve of Eratosthenes

---
+++ 02. Mathematics/sieve_of_eratosthenes.cpp
@@ -0,0 +1,37 @@
+// C++ program to print all primes
+// smaller than or equal to
+// n using Sieve of Eratosthenes
+#include <bits/stdc++.h>
+using namespace std;
+
+void SieveOfEratosthenes(int n)
+{
+
+	bool prime[n + 1];
+	memset(prime, true, sizeof(prime));
+
+	for (int p = 2; p * p <= n; p++)
+	{
+		// If prime[p] is not changed,
+		// then it is a prime
+		if (prime[p] == true)
+		{
+			for (int i = p * p; i <= n; i += p)
+				prime[i] = false;
+		}
+	}
+
+	for (int p = 2; p <= n; p++)
+		if (prime[p])
+			cout << p << " ";
+}
+
+// Driver Code
+int main()
+{
+	int n = 30;
+	cout << "Following are the prime numbers smaller "
+		<< " than or equal to " << n << endl;
+	SieveOfEratosthenes(n);
+	return 0;
+}
