fa07d134b9bd7c7cc22c7d99cfd275854a0a8cba
Divyesh Khuva <75933218+Divyesh1511@users.noreply.github.com>
2021-10-01 07:51:26 +0530

    Update 1. Remove duplicate elements from sorted Array.cpp

--- 05. Arrays/1. Remove duplicate elements from sorted Array.cpp
+++ 05. Arrays/1. Remove duplicate elements from sorted Array.cpp
@@ -46,7 +46,22 @@ class Solution{
 public:
     int remove_duplicate(int a[],int n){
         // code here
+     if(n==0||n==1)
+      return n;
+     int temp[n];
+     
+     int j=0;
+     for(int i=0; i<n-1;i++){
+      if(a[i]!=a[i+1]){
+       temp[j++] = a[i];
+      }
     }
+         temp[j++] = a[n-1];
+        for (int i=0; i<j; i++){
+        a[i] = temp[i];
+        } 
+    return j; 
+   }
 };
 
 // { Driver Code Starts.
