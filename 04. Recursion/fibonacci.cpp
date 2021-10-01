9b7303ec781090aa9fdd323600808641ae0ad91c
GarvitSinghal47 <82756460+GarvitSinghal47@users.noreply.github.com>
2021-10-01 16:19:03 +0530

    code for fibonacci  in recursion
    added code for fibonacci question using recursion plz add and accept it under hactoberfest

---
+++ 04. Recursion/fibonacci.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int fib(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    if (n == 1)
+    {
+        return 1;
+    }
+    int smalloutput1 = fib(n - 1);
+    int smalloutput2 = fib(n - 2);
+    return smalloutput1 + smalloutput2;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << fib(n) << endl;
+}
