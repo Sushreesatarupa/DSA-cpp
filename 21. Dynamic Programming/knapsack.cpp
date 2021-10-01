d587dcdef6b7b394210a2c9e8d44b03cbf535215
Abhay Charan Patro <abhaycharanpatro@gmail.com>
2021-10-01 17:59:37 +0530

    Create knapsack.cpp

---
+++ 21. Dynamic Programming/knapsack.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int knapSack(int W, int wt[], int val[], int n)
+{
+	int i, w;
+	vector<vector<int>> K(n + 1, vector<int>(W + 1));
+	for(i = 0; i <= n; i++)
+	{
+		for(w = 0; w <= W; w++)
+		{
+			if (i == 0 || w == 0)
+				K[i][w] = 0;
+			else if (wt[i - 1] <= w)
+				K[i][w] = max(val[i - 1] +
+								K[i - 1][w - wt[i - 1]],
+								K[i - 1][w]);
+			else
+				K[i][w] = K[i - 1][w];
+		}
+	}
+	return K[n][W];
+}
+int main()
+{
+	int val[] = { 34, 12, 30 };
+	int wt[] = { 1, 4, 5 };
+	int W = 50;
+	int n = sizeof(val) / sizeof(val[0]);
+	cout << knapSack(W, wt, val, n);
+	return 0;
+}
