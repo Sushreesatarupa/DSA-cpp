6f09b195c29dd93bbf1d3821bed6d88feaa18bef
Kumar Shashank <34642119+1711shashank@users.noreply.github.com>
2021-10-01 22:40:18 +0530

    subset of a string

---
+++ subset of a string.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                          int
+#define pb                          push_back
+#define ff                          first
+#define ss                          second
+
+#define mem(arr,val)                memset(arr, val, sizeof(arr))
+#define pd(n,l)                     cout<<setprecision(l)<<n
+#define lcm(a,b)                    (a/__gcd(a,b))*b
+
+#define fl(i,a,b)                   for(ll i=a; i<b; i++)
+#define fr(i,a,b)                   for(ll i=b-1; i>=a; i--)
+
+#define ins(s)                      string s;          cin>>s;
+#define in1(a)                      ll a;              cin>>a;
+#define in2(a, b)                   ll a, b;           cin >> a >> b;
+#define in3(a, b, c)                ll a, b, c;        cin >> a >> b >> c;
+#define in4(a, b, c, d)             ll a, b, c, d;     cin >> a >> b >> c >> d;
+
+#define InVec(v,n)                  vector<ll> v(n); fl(i,0,n) {cin>>v[i];}
+#define InArr(a,n)                  ll a[n]; fl(i,0,n) {cin>>a[i];}
+#define InArrSum(a,n,sum)           ll a[n]; fl(i,0,n) {cin>>a[i]; sum += a[i];}
+#define InVecSum(v,n,sum)           vector<ll> v(n); fl(i,0,n) {cin>>v[i]; sum += v[i];}
+
+#define sz(s)                       s.size()
+
+#define dsc                         greater<ll>()
+#define all(x)                      (x).begin(), (x).end()
+
+#define pln(n)                      cout << n << "\n"   
+#define pls(n)                      cout << n << " "   
+#define pt(n)                       cout << n
+#define ln                          cout << "\n"   
+
+#define pmap(mp)                    for(auto i: mp) {p2(i.ff,i.ss);}
+#define printArrVec(a)              for(auto it:a)  {cout<<it<<" ";} ln;
+#define p1(a);                      cout<< a << "\n";   
+#define p2(a,b);                    cout<< a <<" "<< b << "\n";      
+#define p3(a,b,c);                  cout<< a <<" "<< b <<" "<< c << "\n";
+#define YES                         pln("YES")
+#define Yes                         pln("Yes")
+#define NO                          pln("NO")
+#define No                          pln("No")
+
+#define mod 1000000007
+
+
+//subset of a string
+vector<string> subset(string s){
+    vector<string> v;
+    ll n = s.size();
+
+    for(ll p=0; p<=1<<n; p++){
+        string st="";
+
+        for(ll j=0;j<n;j++){
+            if(p & (1<<j)){
+                st += s[j];
+            }
+        }
+        v.pb(st);
+    }
+
+    return v;
+}
+
+void solve(){
+    
+    string s = "abc";
+    vector<string> v;
+    v = subset(s);
+
+    printArrVec(v);
+}
+
+//int32_t 
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+
+    //in1(T);  while(T--)  
+        solve();
+        
+    return 0;
+    // return 0LL;
+}
